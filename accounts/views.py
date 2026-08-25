ab90d433f2d6297280559fa59a2577a36473eb52
adeeltahirdev <adeeltahir0301@gmail.com>
2026-08-25 19:47:24 +0500

    Working on account views

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def login(request):
     return render(request, 'auth/login.html')
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        # username, email and password validation logic
+        # new user creation logic
+    
+    return render(request, 'auth/register.html')
